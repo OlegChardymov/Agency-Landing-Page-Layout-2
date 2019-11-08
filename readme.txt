a0763be60f1c6cb5f0beb4577314511e4939b8fa
Oleg Chardymov <54996009+OlegChardymov@users.noreply.github.com>
2019-11-08 10:57:47 +0300

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,7 @@
+This is a simple agency landing page layout + second page for all projects
+
+There is:
+- hover effects;
+- JS script for sliders
+
+HTML, CSS (SCSS), JS, Avocode
---
+++ script.js
@@ -0,0 +1,53 @@
+const slides = document.getElementsByClassName("slider__item");
+const dots = document.getElementsByClassName("slider__dot");
+
+let slideIndex = 1;
+showSlides(slideIndex);
+autoSlider();
+
+function plusSlide() {
+  showSlides((slideIndex += 1));
+}
+
+function minusSlide() {
+  showSlides((slideIndex -= 1));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function autoSlider() {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(autoSlider, 8000);
+}
